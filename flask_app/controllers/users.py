c4ff5ec444eca34fb2f9a709f523e7bdc074f565
Aden Smith <adensmithy13@gmail.com>
2022-02-18 14:55:01 -0800

    fixes

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, request, session
 
 @app.route("/users/")
 def index():
-    # call the get all classmethod to get all friends
+
     users = User.get_all()
     print(users)
     return render_template("read_all.html", users = users)
@@ -13,17 +13,17 @@ def index():
 def new_user():
     return render_template("create.html")
 
-# relevant code snippet from server.py
+
 @app.route('/users/new/new', methods=["POST"])
 def create_friend():
-# First we make a data dictionary from our request.form coming from our template.
-# The keys in data need to line up exactly with the variables in our query string.
+
+
     data = {
     "first_name": request.form["first_name"],
     "last_name" : request.form["last_name"],
     "email" : request.form["email"]
     }
-# We pass the data dictionary into the save method from the Friend class.
+
     User.save(data)
-# Don't forget to redirect after saving to the database.
+
     return redirect('/users/')
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,6 +1,6 @@
-# import the function that will return an instance of a connection
+
 from flask_app.config.mysqlconnection import connectToMySQL
-# model the class after the friend table from our database
+
 
 DATABASE = "users_schema"
 
@@ -12,16 +12,16 @@ class User:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-# Now we use class methods to query our database
+
 
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-# make sure to call the connectToMySQL function with the schema you are targeting.
+
         results = connectToMySQL(DATABASE).query_db(query)
-# Create an empty list to append our instances of friends
+
         users = []
-# Iterate over the db results and create instances of friends with cls.
+
         for user in results:
             users.append( cls(user) )
         return users
